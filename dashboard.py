0e51e80de8e859ac57a807bcb87a171985aeb92d
SyafiqMSI <muhammadsyafiqibra@gmail.com>
2024-12-11 08:18:09 +0700

    feat(idi): dashboard

--- dashboard.py
+++ dashboard.py
@@ -9,12 +9,12 @@ import nltk
 nltk.download('stopwords', quiet=True)
 from nltk.corpus import stopwords
 
-st.set_page_config(page_title='OJK Survey Dashboard', layout='wide')
+st.set_page_config(page_title='Survey Dashboard', layout='wide', page_icon="📊")
 
 @st.cache_data
-def load_data():
+def load_data(file_path, separator=';', encoding='utf-8'):
     try:
-        df = pd.read_csv('data/hasil/main_data.csv', sep=';', encoding='utf-8')
+        df = pd.read_csv(file_path, sep=separator, encoding=encoding)
         return df
     except Exception as e:
         st.error(f"Error loading data: {e}")
@@ -36,6 +36,18 @@ def get_keyword_options(df, columns, stop_words):
     keyword_counts = Counter(all_keywords)
     
     return sorted(keyword_counts.items(), key=lambda x: x[1], reverse=True)
+
+def get_keyword_frequency(df, column, stop_words):
+    all_keywords = []
+    for text in df[column]:
+        all_keywords.extend(extract_keywords(text, stop_words))
+    
+    keyword_counts = Counter(all_keywords)
+    
+    keyword_df = pd.DataFrame(
+        keyword_counts.items(), 
+        columns=['Keyword', 'Frequency']
+    ).sort_values('Frequency', ascending=False)
 
 def extract_action_phrases(text):
     if pd.isna(text):
@@ -115,13 +127,13 @@ def calculate_sentiment_weight(df):
         else:
             return None
     
-    df['NILAI_BOBOT'] = df['Label_Index'].apply(calculate_weight)
+    df['NILAI_SENTIMEN'] = df['Label_Index'].apply(calculate_weight)
     
-    average_bobot_sentimen = df['NILAI_BOBOT'].mean()
+    average_bobot_sentimen = df['NILAI_SENTIMEN'].mean()
     
     return average_bobot_sentimen
 
-def main():
+def internal_eksternal_page():
     st.title('OJK Survey Data Dashboard')
     
     try:
@@ -130,7 +142,7 @@ def main():
         nltk.download('stopwords', quiet=True)
         stop_words = set(stopwords.words('indonesian'))
     
-    df = load_data()
+    df = load_data('data/hasil/main_data.csv')
     
     if not df.empty:
         
@@ -263,7 +275,6 @@ def main():
         st.dataframe(display_df)
         # st.dataframe(search_df)
         
-
         st.header('Word Analysis')
         verb_active_df = analyze_action_phrases(search_df)
         
@@ -312,6 +323,192 @@ def main():
     
     else:
         st.warning('No data loaded. Please check the data file path.')
+        
+def idi_page():
+    st.title('IDI Survey Data Dashboard')
+    idi_df = load_data('data/hasil/main_data_idi.csv')
+    
+    if not idi_df.empty:
+        
+        try:
+            stop_words = set(stopwords.words('indonesian'))
+        except LookupError:
+            nltk.download('stopwords', quiet=True)
+            stop_words = set(stopwords.words('indonesian'))
+            
+        jenis_filter = st.sidebar.multiselect(
+            'Select Jenis',
+            options=idi_df['Jenis'].unique().tolist(),
+            default=idi_df['Jenis'].unique().tolist()
+        )
+        
+        label_filter = st.sidebar.multiselect(
+            'Select Label',
+            options=idi_df['Label'].unique().tolist(),
+            default=idi_df['Label'].unique().tolist()
+        )
+            
+        filtered_df = idi_df[
+            (idi_df['Jenis'].isin(jenis_filter)) &
+            (idi_df['Label'].isin(label_filter))
+        ]
+        
+        idi_keywords = get_keyword_options(
+            idi_df, 
+            ['Kualitas Layanan', 'Hubungan dengan OJK', 'Kualitas SDM', 'Saran'], 
+            stop_words
+        )
+        
+        idi_keyword_filter = st.sidebar.multiselect(
+                'Keywords in IDI',
+                options=[kw[0] for kw in idi_keywords],
+                default=[],  
+                format_func=lambda x: f"{x} ({dict(idi_keywords)[x]} times)"
+        )
+        
+        if idi_keyword_filter:
+            filtered_df = filtered_df[
+                filtered_df.apply(
+                    lambda row: any(
+                        kw in extract_keywords(row['Kualitas Layanan'], stop_words) or 
+                        kw in extract_keywords(row['Hubungan dengan OJK'], stop_words) or
+                        kw in extract_keywords(row['Kualitas SDM'], stop_words) or
+                        kw in extract_keywords(row['Saran'], stop_words) 
+                        for kw in idi_keyword_filter
+                    ), 
+                    axis=1
+                )
+            ]
+        
+        keyword_df = get_keyword_frequency(idi_df, 'Combined_Text', stop_words)
+        
+        
+        average_sentiment_weight = calculate_sentiment_weight(idi_df)
+        
+        st.header('Survey Metrics')
+        col2, col3 = st.columns(2)
+        
+        # with col1:
+        #     st.metric('Total Responses', filtered_df.shape[0])
+        
+        with col2:
+            label_counts = filtered_df['Label'].value_counts()
+            st.metric('Dominant Label', label_counts.index[0] if len(label_counts) > 0 else 'N/A')
+         
+        with col3:
+            st.metric('Nilai Bobot Sentimen', f"{round(average_sentiment_weight):.2f}")
+
+        st.header('Visualizations')
+        
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.subheader('Label Distribution')
+            label_fig = px.pie(
+                filtered_df, 
+                names='Label', 
+                title='Distribution of Sentiment Labels'
+            )
+            st.plotly_chart(label_fig)
+        
+        with col2:
+            st.subheader('Data Count by Label')
+            label_count_fig = px.bar(
+                filtered_df['Label'].value_counts().reset_index(), 
+                x='Label', 
+                y='count', 
+                title='Number of Data Points by Label'
+            )
+            st.plotly_chart(label_count_fig)
+        
+        st.header('Detailed Data')
+        search_placeholder = "Search across all columns"
+
+        search_term = st.text_input('Search Tabel Query', placeholder=search_placeholder, key="search_input")
+
+        if search_term:
+            search_df = filtered_df[
+                filtered_df.apply(
+                    lambda row: row.astype(str).str.contains(search_term, case=False).any(),
+                    axis=1
+                )
+            ]
+        else:
+            search_df = filtered_df
+
+        columns_to_exclude = ['New_Label','Confidence']  
+        display_df = search_df.drop(columns=columns_to_exclude, errors='ignore')  
+        st.dataframe(display_df)
+
+        st.header('Word Analysis')
+        verb_active_df = analyze_action_phrases(search_df)
+        
+        if not verb_active_df.empty:
+            st.dataframe(verb_active_df, use_container_width=True)
+            
+            st.subheader('Top Words')
+            top_words_fig = px.bar(
+                verb_active_df.head(10), 
+                x='keyword', 
+                y='jumlah', 
+                title='Top 10 Words'
+            )
+            st.plotly_chart(top_words_fig)
+        else:
+            st.write("No verb or active words found in the filtered dataset.")
+
+        if idi_keyword_filter:
+            st.header('Keyword Analysis')
+            
+            kw_freq_data = []
+            for kw in idi_keyword_filter:
+                oq1_count = filtered_df['Kualitas Layanan'].apply(
+                    lambda x: kw in extract_keywords(x, stop_words)
+                ).sum()
+                oq2_count = filtered_df['Hubungan dengan OJK'].apply(
+                    lambda x: kw in extract_keywords(x, stop_words)
+                ).sum()
+                oq3_count = filtered_df['Kualitas SDM'].apply(
+                    lambda x: kw in extract_keywords(x, stop_words)
+                ).sum()
+                oq4_count = filtered_df['Saran'].apply(
+                    lambda x: kw in extract_keywords(x, stop_words)
+                ).sum()
+                
+                kw_freq_data.append({
+                    'Keyword': kw,
+                    'Kualitas Layanan Count': oq1_count,
+                    'Hubungan dengan OJK Count': oq2_count,
+                    'Kualitas SDM Count': oq3_count,
+                    'Saran Count': oq4_count
+                })
+            
+            kw_freq_df = pd.DataFrame(kw_freq_data)
+            
+            st.dataframe(kw_freq_df, use_container_width=True)
+            
+            fig = go.Figure(data=[
+                go.Bar(name='Kualitas Layanan', x=kw_freq_df['Keyword'], y=kw_freq_df['Kualitas Layanan Count']),
+                go.Bar(name='Hubungan dengan OJK', x=kw_freq_df['Keyword'], y=kw_freq_df['Hubungan dengan OJK Count']),
+                go.Bar(name='Kualitas SDM', x=kw_freq_df['Keyword'], y=kw_freq_df['Kualitas SDM Count']),
+                go.Bar(name='Saran Question 2', x=kw_freq_df['Keyword'], y=kw_freq_df['Saran Question 2 Count'])
+            ])
+            fig.update_layout(barmode='group', title='Keyword Frequencies')
+            st.plotly_chart(fig)
+    
+    else:
+        st.warning('No IDI data loaded. Please check the data file path.')
+
+pages = {
+    "INTERNAL-EKSTERNAL": internal_eksternal_page,
+    "IDI Dashboard": idi_page
+}
+
+def main():
+    st.sidebar.title("Navigation")
+    page = st.sidebar.selectbox("Select a Page", list(pages.keys()))
+    
+    pages[page]()
 
 if __name__ == '__main__':
     main()
